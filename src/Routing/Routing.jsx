a188d164eb159e57e9a11aa3ecd7914ea0c5754c
rjspyk5 <rjspyk5@gmail.com>
2024-11-14 12:51:44 +0600

    setup simple router

---
+++ src/Routing/Routing.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export const Routing = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
