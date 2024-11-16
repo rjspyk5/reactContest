2a9808df70baa4eea014eecf16aef5b2319f8412
rjspyk5 <rjspyk5@gmail.com>
2024-11-16 23:58:10 +0600

    setup basic navbar

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Rootlayout } from "./layout/Rootlayout";
+import { Home } from "./Pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
 
 export const Routing = createBrowserRouter([
   {
     path: "/",
     element: <Rootlayout />,
+    children: [{ path: "", element: <Home /> }],
   },
 ]);
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../Pages/shared/Navbar/Navbar";
+import { Footer } from "../Pages/shared/Footer/Footer";
 
 export const Rootlayout = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
