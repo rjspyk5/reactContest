daddbc8f35a961adf753e9744dc7dc3d0fbe6ab0
rjspyk5 <rjspyk5@gmail.com>
2024-11-17 22:50:07 +0600

    add basic routing

---
+++ src/Pages/shared/TopHeader/TopHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const TopHeader = () => {
+  return (
+    <div className="flex gap-10">
+      <marquee className="flex-1" direction="left">
+        This is test
+      </marquee>{" "}
+      <div>Call for Booking 01790099988</div>
+    </div>
+  );
+};
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Pages/shared/Navbar/Navbar";
 import { Footer } from "../Pages/shared/Footer/Footer";
+import { TopHeader } from "../Pages/shared/TopHeader/TopHeader";
 
 export const Rootlayout = () => {
   return (
     <div>
+      <TopHeader />
       <Navbar />
       <Outlet />
       <Footer />
