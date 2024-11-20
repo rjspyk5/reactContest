2cd50faec4902a428f5a0a2e08638561e31fabad
rjspyk5 <rjspyk5@gmail.com>
2024-11-21 00:09:40 +0600

    add basic tabs group

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Hero } from "../../components/HeroSection/Hero";
 import { PopularServices } from "../../components/PopularServices/PopularServices";
+import { AllServices } from "../../components/AllServices/AllServices";
 
 export const Home = () => {
   return (
     <div>
       <Hero />
       <PopularServices />
+      <AllServices />
     </div>
   );
 };
