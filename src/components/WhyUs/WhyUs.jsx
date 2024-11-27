298937ca5ffb3367d0cebfa839b8fdcf61cb808f
rjspyk5 <rjspyk5@gmail.com>
2024-11-27 16:02:07 +0600

    make responsive why us section

--- src/components/WhyUs/WhyUs.jsx
+++ src/components/WhyUs/WhyUs.jsx
@@ -14,70 +14,70 @@ export const WhyUs = () => {
         }}
       >
         <div className="hero-overlay w-full h-full bg-opacity-50 bg-black"></div>
-        <div className="flex flex-col md:flex-row gap-20 md:p-20 p-10">
+        <div className="flex flex-col lg:flex-row gap-16 md:p-10  p-3">
           <div className="w-full lg:w-50">
             <p
-              style={{ lineHeight: "4rem" }}
+              style={{ lineHeight: "3rem" }}
               className="text-2xl md:text-3xl lg:text-5xl font-semibold text-white "
             >
               Expertise on your doorstep, covering the whole of the UK.
             </p>
           </div>
           <div className="w-full lg:w-50">
-            <div className="rounded grid grid-cols-2 *:rounded-lg gap-10 md:gap-16 lg:gap-20">
-              <div className="flex-col items-center justify-center space-y-5 bg-white p-10">
+            <div className="rounded grid grid-cols-2 *:rounded-lg gap-5 md:gap-10 ">
+              <div className="flex-col items-center justify-center space-y-5 bg-white p-8 md:p-10">
                 <div className="flex justify-center">
                   <IoHome
                     className="text-green-500 text-center block mt-5"
                     size={50}
                   />
                 </div>
-                <p className="font-semibold text-green-500 text-5xl text-center">
+                <p className="font-semibold text-green-500 md:text-5xl text-3xl text-center">
                   2000+
                 </p>
-                <p className="leading-6 text-center text-lg opacity-70">
+                <p className="leading-6 text-center text-base  md:text-lg opacity-70">
                   5-star ratings on Trustpilot across our divisions
                 </p>
               </div>
-              <div className="flex-col items-center justify-center space-y-5 bg-white p-10">
+              <div className="flex-col items-center justify-center space-y-5 bg-white p-8 md:p-10">
                 <div className="flex justify-center">
                   <IoHome
                     className="text-green-500 text-center block mt-5"
                     size={50}
                   />
                 </div>
-                <p className="font-semibold text-green-500 text-5xl text-center">
+                <p className="font-semibold text-green-500 md:text-5xl text-3xl text-center">
                   2000+
                 </p>
-                <p className="leading-6 text-center text-lg opacity-70">
+                <p className="leading-6 text-center text-base  md:text-lg opacity-70">
                   5-star ratings on Trustpilot across our divisions
                 </p>
               </div>
-              <div className="flex-col items-center justify-center space-y-5 bg-white p-10">
+              <div className="flex-col items-center justify-center space-y-5 bg-white p-8 md:p-10">
                 <div className="flex justify-center">
                   <IoHome
                     className="text-green-500 text-center block mt-5"
                     size={50}
                   />
                 </div>
-                <p className="font-semibold text-green-500 text-5xl text-center">
+                <p className="font-semibold text-green-500 md:text-5xl text-3xl text-center">
                   2000+
                 </p>
-                <p className="leading-6 text-center text-lg opacity-70">
+                <p className="leading-6 text-center text-base  md:text-lg opacity-70">
                   5-star ratings on Trustpilot across our divisions
                 </p>
               </div>
-              <div className="flex-col items-center justify-center space-y-5 bg-white p-10">
+              <div className="flex-col items-center justify-center space-y-5 bg-white p-8 md:p-10">
                 <div className="flex justify-center">
                   <IoHome
                     className="text-green-500 text-center block mt-5"
                     size={50}
                   />
                 </div>
-                <p className="font-semibold text-green-500 text-5xl text-center">
+                <p className="font-semibold text-green-500 md:text-5xl text-3xl text-center">
                   2000+
                 </p>
-                <p className="leading-6 text-center text-lg opacity-70">
+                <p className="leading-6 text-center text-base  md:text-lg opacity-70">
                   5-star ratings on Trustpilot across our divisions
                 </p>
               </div>
