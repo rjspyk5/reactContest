b47e57b6ec36793d40a3837ac73a96fbc267daad
rjspyk5 <rjspyk5@gmail.com>
2024-12-09 22:26:01 +0600

    simple login page created

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Rootlayout } from "./layout/Rootlayout";
 import { Home } from "./Pages/Home/Home";
 import { createBrowserRouter } from "react-router-dom";
+import { Login } from "./Pages/Login/Login";
 
 export const Routing = createBrowserRouter([
   {
     path: "/",
     element: <Rootlayout />,
-    children: [{ path: "", element: <Home /> }],
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ]);
