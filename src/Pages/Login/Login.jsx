940095e9f0159048030de104755cbba557b8bfc7
rjspyk5 <rjspyk5@gmail.com>
2024-12-14 16:23:03 +0600

    Registration Form desgin Complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,26 +1,33 @@
 import React from "react";
+import img from "../../assets/watcing.jpg";
 
 export const Login = () => {
   return (
     <div>
       <div className="font-sans">
-        <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 ">
-          <div className="relative sm:max-w-sm w-full">
-            <div className="card bg-blue-400 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
-            <div className="card bg-red-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
-            <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+        <div
+          style={{ backgroundImage: `url(${img})` }}
+          className="relative min-h-screen flex flex-col bg-no-repeat bg-center bg-cover  sm:justify-center items-center bg-gray-100 "
+        >
+          <div className="absolute  w-full h-full  bg-black bg-opacity-40"></div>
+          {/* <img src={img} className="w-full h-full absolute blur-[5px]" alt="" /> */}
+
+          <div className="relative sm:max-w-md w-full mt-28 mb-10">
+            <div className="card bg-[#4a494923] backdrop-blur-xl   shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
+            <div className="card bg-[#29292923] backdrop-blur-xl  shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
+            <div className="relative w-full rounded-3xl   px-6 py-4 bg-[#d7d7d70a] backdrop-blur shadow-md">
               <label
                 for=""
-                className="block mt-3 text-sm text-gray-700 text-center font-semibold"
+                className="block mt-3 text-white text-xl text-center font-semibold"
               >
                 Registration
               </label>
-              <form method="#" action="#" className="mt-10">
+              <form className="mt-5">
                 <div>
                   <input
                     type="text"
                     placeholder="Your Name"
-                    className="mt-1 p-2 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                    className="mt-1 p-2 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 focus:outline-none placeholder-gray-400 text-slate-900"
                   />
                 </div>
 
@@ -28,7 +35,7 @@ export const Login = () => {
                   <input
                     type="email"
                     placeholder="Enter Your Email"
-                    className="mt-1 block p-2 w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                    className="mt-1 block p-2 w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 focus:outline-none placeholder-gray-400 text-slate-900"
                   />
                 </div>
 
@@ -36,20 +43,20 @@ export const Login = () => {
                   <input
                     type="password"
                     placeholder="Enter Your Password"
-                    className="mt-1 p-2 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                    className="mt-1 p-2 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 focus:outline-none placeholder-gray-400 text-slate-900"
                   />
                 </div>
 
                 <div className="mt-7">
-                  <button className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                  <button className="bg-green-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
                     Registrar
                   </button>
                 </div>
 
                 <div className="flex mt-7 items-center text-center">
                   <hr className="border-gray-300 border-1 w-full rounded-md" />
-                  <label className="block font-medium text-sm text-gray-600 w-full">
-                    Register with
+                  <label className="block font-medium text-sm text-white w-full">
+                    Or Register with
                   </label>
                   <hr className="border-gray-300 border-1 w-full rounded-md" />
                 </div>
@@ -66,12 +73,12 @@ export const Login = () => {
 
                 <div className="mt-7">
                   <div className="flex justify-center items-center">
-                    <label className="mr-2">¿Ya tienes una cuenta?</label>
+                    <label className="mr-2">Already have an account?</label>
                     <a
                       href="#"
                       className=" text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
                     >
-                      Iniciar sesion
+                      Login Now
                     </a>
                   </div>
                 </div>
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "../Pages/shared/Navbar/Navbar";
 import { Footer } from "../Pages/shared/Footer/Footer";
 import { TopHeader } from "../Pages/shared/TopHeader/TopHeader";
 
 export const Rootlayout = () => {
+  const { pathname } = useLocation();
+
   return (
     <div>
-      <TopHeader />
+      {pathname === "/" && <TopHeader />}
+
       <Navbar />
       <Outlet />
       <Footer />
