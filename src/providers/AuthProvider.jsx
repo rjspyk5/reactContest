6c28f6e49515ad6ab4cdd2ef9a091a093962adc8
rjspyk5 <rjspyk5@gmail.com>
2024-12-15 23:24:22 +0600

    make login,reg,googlelog in function

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,12 @@
 import React, { createContext, useEffect, useState } from "react";
 import { auth } from "./../../firebase.config";
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signInWithPopup,
+} from "firebase/auth";
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [user, setuser] = useState(null);
@@ -8,18 +14,30 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (userr) => {
-      console.log(userr);
+      setuser(userr);
+      setloading(false);
     });
     return () => {
       unsubscribe();
     };
   }, []);
 
-  const signIn = () => {};
-  const logIn = () => {};
-  const logInWithGoogle = () => {};
+  const signUp = (email, pass) => {
+    setloading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
+  const logIn = (email, pass) => {
+    setloading(true);
+    return signInWithCredential(auth, email, pass);
+  };
+
+  const provider = new GoogleAuthProvider();
+  const logInWithGoogle = () => {
+    setloading(true);
+    return signInWithPopup(auth, provider);
+  };
   const context = {
-    signIn,
+    signUp,
     logIn,
     logInWithGoogle,
     user,
