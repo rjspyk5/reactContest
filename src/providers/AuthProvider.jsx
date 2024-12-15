91fd53eeb968075d67208117bb6726e38421b6a0
rjspyk5 <rjspyk5@gmail.com>
2024-12-15 23:16:47 +0600

    authprovider setup complete

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,31 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
+import { auth } from "./../../firebase.config";
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [user, setuser] = useState(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userr) => {
+      console.log(userr);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const signIn = () => {};
+  const logIn = () => {};
+  const logInWithGoogle = () => {};
+  const context = {
+    signIn,
+    logIn,
+    logInWithGoogle,
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
 };
